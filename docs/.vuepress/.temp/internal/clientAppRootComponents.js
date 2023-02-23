48cab81bdfc0f58d6a04d9e3f645cd2da94afe01
陈凯文 <chenkaiwen@linklogis.com>
2023-02-23 13:37:38 +0800

    fix: 删除没用文件

--- docs/.vuepress/.temp/internal/clientAppRootComponents.js
+++
@@ -1,5 +0,0 @@
-import clientAppRootComponent0 from '/Users/keywen/Documents/study/github/kaiziwen/webpack5/node_modules/@vuepress/plugin-back-to-top/lib/client/components/BackToTop.js'
-
-export const clientAppRootComponents = [
-  clientAppRootComponent0,
-]
